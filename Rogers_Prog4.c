5ba80c7b1197d489200ae8b46419a0b538a43164
Jeremy Rogers <152228726+hullucid@users.noreply.github.com>
2023-12-13 11:00:38 -0600

    Add files via upload

--- Rogers_Prog4.c
+++ Rogers_Prog4.c
@@ -7,16 +7,20 @@ int main() {
 	int months; // declarations 
 	float loanAmount, monthlyPayment, loanBalance;
 	int i = 1;
+	
 	//asking user for the loan amount 
 	printf ("Enter the loan amount: ");
 	scanf ("%f" , &loanAmount);
+	
 	//asking use for the monthly payment amount 
 	printf ("Enter the monthly payment: ");
 	scanf ("%f" , &monthlyPayment);
-	//formula for calculating months 
+	
+	//formula for calculating months
 	months = loanAmount/monthlyPayment;
 	printf ("It will take approximately %d months \n" , months);
-	//loop for printing how many months it will take for loan balance to be paid off 	
+	
+	//loop for printing the balance each time a monthly payment is made 	
 	while (loanAmount != 0 )
 	{
 		
@@ -29,6 +33,7 @@ int main() {
 	//users loan has been paid off 
 	printf ("Congrats you have paid off your loan!");
 	
+	system("pause");
 	
 	return 0;
 }
